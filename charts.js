e27e8b11473808f57b1c1c9fc6eaa7751bf56725
Kaila Person <kailaperson@Kailas-MacBook-Air.local>
2022-05-26 15:23:25 -0700

    update js file

--- charts.js
+++ charts.js
@@ -72,7 +72,7 @@ function buildCharts(sample) {
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = id.slice(0,10).sort((a.b) => b - a).reverse();
+    var yticks = id.slice(0,10).sort((a,b) => b - a).reverse();
 
     // 8. Create the trace for the bar chart. 
     var barData = [{
