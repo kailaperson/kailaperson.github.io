d28bd7f2b8ab7d8f767db89d54d1774b40d38ca3
Kaila Person <kailaperson@Kailas-MacBook-Air.local>
2022-05-27 12:23:45 -0700

    update

--- charts.js
+++ charts.js
@@ -85,6 +85,6 @@ function buildCharts(sample) {
      title: "Top 10 Bacteria Cultures Found"
     };
     // 10. Use Plotly to plot the data with the layout. 
-    Plotly.newPlot("bar-plot", barData, barLayout);
+  Plotly.newPlot("bar-plot", barData, barLayout);
   });
 }
